f971f8da9782f11fafb87d3e7af72fa1d499906d
GardeisenChawaf <marine.gardeisen@free.fr>
2019-10-07 10:44:02 +0200

    create parseur

---
+++ src/main/scala/Parseur.scala
@@ -0,0 +1,48 @@
+import java.io.File
+
+import scopt.OParser
+case class Config(
+                   foo: Int = -1,
+                   out: File = new File("."),
+                   xyz: Boolean = false,
+                   libName: String = "",
+                   maxCount: Int = -1,
+                   verbose: Boolean = false,
+                   debug: Boolean = false,
+                   mode: String = "",
+                   files: Seq[File] = Seq(),
+                   keepalive: Boolean = false,
+                   jars: Seq[File] = Seq(),
+                   kwargs: Map[String, String] = Map())
+
+
+
+object Parseur extends App {
+
+  val builder = OParser.builder[Config]
+  val parser1 = {
+    import builder._
+    OParser.sequence(
+      programName("sGit"),
+      head("sGit", "1.0"),
+
+      cmd("init")
+        .action((_, c) => c.copy(mode = "init"))
+        .text("create sgit repository.")
+
+
+    )
+  }
+
+  // OParser.parse returns Option[Config]
+  OParser.parse(parser1, args, Config()) match {
+    case Some(config) =>
+      config.mode match {
+        case "init" =>
+          println("on test le parseur ")
+      }
+    case _ =>
+    // argum
+
+  }
+}
