e4eae97d6a5919ea16ab43afe87a9472b27693fa
GardeisenChawaf <marine.gardeisen@free.fr>
2019-10-12 20:09:41 +0200

    preparation of the tests for init

---
+++ src/test/scala/sgit/InitTest.scala
@@ -0,0 +1,35 @@
+package sgit
+
+import org.scalatest.{BeforeAndAfter, FunSpec, GivenWhenThen, Matchers}
+
+class InitTest extends FunSpec with Matchers with GivenWhenThen with BeforeAndAfter{
+
+  //TESTS FOR THE INIT
+  describe("When I run the init command"){
+      //it("Should create a file HEAD"){}
+      //TO DO
+
+    //it("Should create a folder .sgit"){}
+
+    //it("Should create a file .sgit/objects"){}
+
+    //it("Should create a file .sgit/branches"){}
+
+    //it("Should create a file .sgit/tags"){}
+
+    //it("Should create a file .sgit/objects/blob"){}
+
+    //it("Should create a file .sgit/objects/tree"){}
+
+    //it("Should create a file .sgit/branches"){}
+
+    //it("Should print already initialized if a repo already exist"){}
+
+
+
+
+
+
+  }
+
+}
