3efb506bb84aa48c171592f20c99bbf0ba24fb4e
GardeisenChawaf <marine.gardeisen@free.fr>
2019-10-12 21:49:08 +0200

    migrate function regarding I/O to another file

--- src/test/scala/sgit/AddTest.scala
+++ src/test/scala/sgit/AddTest.scala
@@ -4,26 +4,35 @@ import org.scalatest.{BeforeAndAfter, FunSpec, GivenWhenThen, Matchers}
 
 class AddTest extends FunSpec with Matchers with GivenWhenThen with BeforeAndAfter {
 
-  //TEST FOR "createBlob" FUNCTION
-  describe("???") {
-    describe("????") {
-      //it(""){}
-      //TO DO
+
+  describe("Given a file test.md to add") {
+    //create the file test
+
+    describe("Run the function blob"){
+      //it("Should create a file name 'README.md content hash' "){}
+      //TODO
+      //it("Should create a file content the same content of the file README.md"){}
+    }
+
+    describe("Run the function add for test.md"){
+      //it("Should print unknown if file doesn't exist"){}
+      //TODO
+      //WARNING mock INDEX ??? Possible ?? comment ne pas ecrire dans le vrai index
+      //it("Should add to a file INDEX a line : test.md and the blob associate"){}
+      //TODO
+    }
+    describe("Run the function add for test.md which have been modified"){
+      //modified the content of the file test
+      //it("Should create a nwe blob"){}
+      //TODO
+      //it("Should transform the line in the index with the new blob"){}
+      //TODO
+    }
+
+    describe("Run the function add for test.md and testTwo.md :  ie a array of folder"){
+      //it("Should write two line in the INDEX"){}
+      //TODO
     }
   }
 
-  //TEST FOR "addOneFile" FUNCTION
-  describe("???") {
-    describe("????") {
-      //it("")
-      //TO DO
-    }
-  }
-  //TEST FOR "add" FUNCTION
-  describe("???") {
-    describe("????") {
-      //it("")
-      //TO DO
-    }
-  }
 }
