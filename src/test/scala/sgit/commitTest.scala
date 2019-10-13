3114c41ad9ff8ff4f71293a39416ce65d1395f6e
GardeisenChawaf <marine.gardeisen@free.fr>
2019-10-13 17:04:08 +0200

    begin of impl for commit test

--- src/test/scala/sgit/commitTest.scala
+++ src/test/scala/sgit/commitTest.scala
@@ -5,8 +5,12 @@ import java.io.File
 import org.scalatest._
 import sgit.UtilityGit._
 
+import scala.reflect.io.Directory
+
 //TESTS FOR THE FUNCTION OF TRANSFORMATION
 class commitTest extends FunSpec with Matchers with GivenWhenThen with BeforeAndAfter {
+
+  val path = System.getProperty("user.dir")
 
   describe("Given an array of array of Stings") {
     val list = Array(Array("marine", "gardeisen", "3"), Array("mathieu", "gardeisen", "4"), Array("2"), Array("marine", "4"))
@@ -20,42 +24,63 @@ class commitTest extends FunSpec with Matchers with GivenWhenThen with BeforeAnd
 
   describe("Given a File, looks like the Index") {
 
-    //val INDEX = new File(System.getProperty("user.dir") + "/src/test/scala/sgit/indexTest")
-    //val content = "README.md 564z16z328fe42fz6 \n src/main/scala/sgit/Add.scala ferg555zz5fr95vz64f869er8z"
-    //INDEX.createNewFile()
-    //writeInAFile(INDEX, content)
-
     //TESTS FOR "createTableOfPath" function
     describe("I want to get the table of path associate ie: for one cell get a word of the path") {
       it("Should give a list of size 2") {
-        //TO DO
+        Init.init()
+        Add.add(Seq[File](new File(path+"/README.md")))
+        val index = new File(path + "/.sgit/INDEX")
+        val tablePath = Commit.createTableOfPath(index)
+        tablePath.length shouldBe 1
       }
-      it("Should put README.md in the first element of the first array") {
-        //TO DO
+      it("Should put src the first element of the first array") {
+        Init.init()
+        Add.add(Seq[File](new File(path+"/src/main/scala/sgit/Add.scala")))
+        val index = new File(path + "/.sgit/INDEX")
+        val tablePath = Commit.createTableOfPath(index)
+        tablePath.apply(0).apply(0) shouldBe "README.md"
+
+        new Directory(new File(path + "/.sgit")).deleteRecursively()
       }
-      it("Should put main in the second element of the second array") {
-        //TO DO
+      it("Should put sgit in the fourth element of the first array") {
+        Init.init()
+        Add.add(Seq[File](new File(path+"/src/main/scala/sgit/Add.scala")))
+        val index = new File(path + "/.sgit/INDEX")
+        val tablePath = Commit.createTableOfPath(index)
+        tablePath.apply(0).apply(0) shouldBe "README.md"
+
+        new Directory(new File(path + "/.sgit")).deleteRecursively()
       }
     }
     //TEST FOR THE "createMapIndex" FUNCTION
     describe("I want to get the map which associate the index to the ref blob") {
       it("Should give a map with two keys") {
-        //TO DO
+        Init.init()
+        Add.add(Seq[File](new File(path+path+"/README.md")))
+        val index = new File(path + "/.sgit/INDEX")
+        val mapIndex = Commit.createMapIndex(index)
+        mapIndex.knownSize shouldBe 2
+
+        new Directory(new File(path + "/.sgit")).deleteRecursively()
       }
-      it("Should return 564z16z328fe42fz6 for the key README.md") {
-        //TO DO
+      it("Should return good value (the hash) for the key README.md") {
+        Init.init()
+        val readme = new File(path+"/README.md")
+        Add.add(Seq[File](readme))
+        val index = new File(path + "/.sgit/INDEX")
+        val mapIndex = Commit.createMapIndex(index)
+        mapIndex.get("/README.md") shouldBe sha1Transformation(getContent(readme))
+
+        new Directory(new File(path + "/.sgit")).deleteRecursively()
       }
     }
-
-    //delete the file test
-    //INDEX.deleteOnExit()
   }
 
   //TESTS FOR THE COMMIT
   describe("???"){
     describe("????"){
       //it("")
-      //TO DO
+      //TODO
     }
   }
 
