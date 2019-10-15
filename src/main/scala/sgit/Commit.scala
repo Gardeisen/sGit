4f1cf4448e67c3a80b20e2111e866ba7147a1053
GardeisenChawaf <marine.gardeisen@free.fr>
2019-10-15 17:27:10 +0200

    fixes

--- src/main/scala/sgit/Commit.scala
+++ src/main/scala/sgit/Commit.scala
@@ -36,7 +36,7 @@ object Commit {
   }
 
   /**
-   * Function deepLengthMax, calcul the length max of a list of list
+   * Function deepLengthMax, compute the length max of a list of list
    *
    * @param tab    : the list of list you want to know the max length
    * @param length : use to store the result for the recursion
@@ -57,6 +57,11 @@ object Commit {
     }
   }
 
+  /**
+   * Function getLengthOfElement compute for the string the length as per of the \
+   * @param element : the string we want to compute the size
+   * @return a int correspond to the size
+   */
   def getLengthOfElement(element: String): Int = {
     if (element.isEmpty) {
       0
@@ -69,6 +74,12 @@ object Commit {
     }
   }
 
+  /**
+   * function cutLineByLength is use to segment the string as per of the \
+   * @param line : the string we want to cut
+   * @param length : the index, size at where we want to cut
+   * @return the string cut
+   */
   @tailrec
   def cutLineByLength(line: String, length: Int): String = {
     if (line.isEmpty) {
@@ -82,15 +93,33 @@ object Commit {
     }
   }
 
+  /**
+   * function createListByLength make a new list we the sting of the right size
+   * @param list : the global list
+   * @param length : the size we want to cut
+   * @return a list with the good element to the right size
+   */
   def createListByLength(list: List[String], length: Int): List[String] = {
     list.filter(line => getLengthOfElement(line) >= length).map(line => cutLineByLength(line, length))
   }
 
+  /**
+   * function createMapFromList transform a list into a map of the right size
+   * @param list the list we want to transform
+   * @param length the size of the element we want to make
+   * @return the map
+   */
   def createMapFromList(list: List[String], length: Int): Map[String, List[String]] = {
     list.groupMap(line => cutLineByLength(line, length - 1))(line => line)
   }
 
-
+  /**
+   * function create tree
+   * @param children the folder or the file under the element study
+   * @param path the name of the tree before hash
+   * @param pathToWrite the location where the file will be write
+   * @return a file correspond to the tree
+   */
   def createTree(children: List[String], path: String, pathToWrite: String = System.getProperty("user.dir") + "/.sgit/objects/trees/"): File = {
 
     val tree = new File(pathToWrite + sha1Transformation(path))
@@ -110,12 +139,21 @@ object Commit {
     tree
   }
 
+  /**
+   * function createTreeFromMap create a tree from the map passed in parameter
+   * @param mapUse : the map we want to turn into a tree
+   * @return a list of file which correspond to the different tree
+   */
   def createTreeFromMap(mapUse: Map[String, List[String]]): List[File] = {
 
     mapUse.toList.map(couple => createTree(couple._2, couple._1))
 
   }
 
+  /**
+   * function createTreesFromIndex create all the tree correspond to an Index
+   * @return the last tree build
+   */
   def createTreesFromIndex(): File = {
     val index = new File(System.getProperty("user.dir") + "/.sgit/INDEX")
     val listIndex = createTableOfPath(index)
@@ -133,6 +171,10 @@ object Commit {
     createTreesFromIndexRec(deepLengthMax(listIndex))
   }
 
+  /**
+   * function commit create the file commit contains the ref to the last tree
+   * @return the file correspond to the commit
+   */
   def commit(): File = {
     val lastTree = createTreesFromIndex()
     val content = getContent(lastTree).mkString("\n")
